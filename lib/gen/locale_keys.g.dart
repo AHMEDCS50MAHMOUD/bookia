eecd9075df94f4b3d29ff16983631601f5cb79c7
Ahmed Darwish <ahmed.mahmoud.40rhh@gmail.com>
2026-02-25 02:51:21 +0200

    Api

---
+++ lib/gen/locale_keys.g.dart
@@ -0,0 +1,15 @@
+// DO NOT EDIT. This is code generated via package:easy_localization/generate.dart
+
+// ignore_for_file: constant_identifier_names
+
+abstract class  LocaleKeys {
+  static const hello = 'hello';
+  static const on_boarding_statement = 'on_boarding_statement';
+  static const login = 'login';
+  static const register = 'register';
+  static const welcome_back_glad_to_see_you_Again = 'welcome_back_glad_to_see_you_Again';
+  static const hello_Register_to_get_started = 'hello_Register_to_get_started';
+  static const enter_your_email = 'enter_your_email';
+  static const enter_your_password = 'enter_your_password';
+
+}
